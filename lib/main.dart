4aceb3254abdbfa5b45b97313a9f81b905ff33f6
kason <718087538@qq.com>
2019-09-29 18:44:32 +0800

    首页直播间大概

--- lib/main.dart
+++ lib/main.dart
@@ -6,27 +6,41 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Flutter Demo',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Im Title32'),
-        ),
-          body: new RoomList()
-      )
-    );
+        title: 'Flutter Demo',
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text('Im Tit221e22'),
+            ),
+            body: new RoomList()));
   }
 }
 
-class RoomList extends StatelessWidget{
+//列表
+class RoomList extends StatelessWidget {
   @override
-  Widget build (BuildContext context){
+  Widget build(BuildContext context) {
     return ListView.builder(
-        itemCount: 100,
-        itemExtent: 100.0, //强制高度为50.0
+        itemCount: 10,
+        itemExtent: 200.0, //强制高度为50.0
         itemBuilder: (BuildContext context, int index) {
-          return ListTile(title: Text("$index"));
-        }
+          return new RoomBox();
+        });
+  }
+}
+
+//直播间的容器
+class RoomBox extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Image(
+        image: NetworkImage("https://s2.ax1x.com/2019/09/04/nVUFeS.jpg"),
+        fit: BoxFit.fill,
+
+      ),
+      margin: EdgeInsets.only(bottom: 10.0,left:10.0,right: 10.0),
+      width: 240.0,
+      //高度,list已经设置了
     );
   }
-
 }
