40403c08fd0acab282f4f9bb6adff1eca1f0cba4
kason <718087538@qq.com>
2019-09-29 21:20:17 +0800

    先到这

--- lib/login.dart
+++ lib/login.dart
@@ -7,29 +7,29 @@ class Login extends StatelessWidget {
         appBar: AppBar(
           title: Text('测试页'),
         ),
-        body: new MyForm()
-    );
+        body: new MyForm());
   }
 }
 
-class MyForm extends StatefulWidget{
-  MyForm({Key key}) : super (key :key);
+class MyForm extends StatefulWidget {
+  MyForm({Key key}) : super(key: key);
 
-  _MyForm  createState() => _MyForm();
+  _MyForm createState() => _MyForm();
 }
 
-class _MyForm extends State<MyForm>{
+class _MyForm extends State<MyForm> {
 //var _username = new TextEditingController();//这个方法用于初始化赋值,如果不赋值,下面的方法更好
   var _username;
   var _password;
-  @override
-  void initState(){
-    super.initState();
-    _username.text = '初始值';
-  }
 
   @override
-  Widget build(BuildContext context){
+//  void initState() {
+//    super.initState();
+//    _username.text = '初始值';
+//  }
+
+  @override
+  Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.all(20.0),
       child: Column(
@@ -44,10 +44,14 @@ class _MyForm extends State<MyForm>{
                 labelText: "账号",
               ),
               onChanged: (value) {
-                setState((){//把文本框的值实时赋给一个变量
+                setState(() {
+                  //把文本框的值实时赋给一个变量
                   _username.text = value;
                 });
               }),
+
+          SizedBox(height: 20.0),//高度20的盒子,可以当空行用
+
           TextField(
               obscureText: true, //密码框
               decoration: InputDecoration(
@@ -55,11 +59,11 @@ class _MyForm extends State<MyForm>{
                   hintText: "请输入密码",
                   labelText: "密码"),
               onChanged: (value) {
-                setState((){//把文本框的值实时赋给一个变量
+                setState(() {
+                  //把文本框的值实时赋给一个变量
                   this._password = value;
                 });
-              }
-          ),
+              }),
           SizedBox(height: 20.0),
           Container(
             width: double.infinity, //让按钮宽度自适应
@@ -67,10 +71,8 @@ class _MyForm extends State<MyForm>{
             child: RaisedButton(
               child: Text("登录"),
               onPressed: () {
-                print( this._username.text);//打印输入框的值
-                print(this._password);//打印输入框的值.密码不用加.text
-
-
+                print(this._username.text); //打印输入框的值
+                print(this._password); //打印输入框的值.密码不用加.text
               },
               color: Colors.blue,
               textColor: Colors.white,
@@ -81,4 +83,3 @@ class _MyForm extends State<MyForm>{
     );
   }
 }
-
