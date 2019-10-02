94518835725afba94b6211571d56dbb0a54f33f3
kason <718087538@qq.com>
2019-10-02 15:23:22 +0800

    改造main.dart渲染

--- lib/test.dart
+++ lib/test.dart
@@ -13,30 +13,56 @@ class Login extends StatelessWidget {
     );
   }
 }
-//不好的渲染效果
+
 class HomePage extends StatelessWidget {
-//  final _items = List<Widget>.generate(10,
-//          (i) => Container(padding: EdgeInsets.all(16.0), child: Text("Item $i")));
-//
-//  @override
-//  Widget build(BuildContext context) {
-//    return ListView(
-//      children: _items,
-//    );
-//  }
-  final _items = List<String>.generate(1000, (i) => "Item $i");
+  //不好的渲染效果
+//  final _items = List<Widget>.generate(10, (i) => Container(padding: EdgeInsets.all(16.0), child: Text("Item $i")));
+  final _items = List<Widget>.generate(10, (i) => Container(
+      padding: EdgeInsets.all(16.0),
+      child: Column(
+          children: <Widget>[
+
+            Image.network("https://s2.ax1x.com/2019/09/04/nVUFeS.jpg"),
+            Text("我是标题",
+              style: new TextStyle(
+                color: Colors.white,
+                fontSize: 18.0,
+                letterSpacing: 4.0,
+
+              ),
+            ),
+            Text("我是富标题",
+              style: new TextStyle(
+              color: Colors.white,
+              fontSize: 20.0,
+              letterSpacing: 4.0,
+            ),),
+          ]
+      ))
+  
+  );
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: 1000,
-      itemBuilder: (context, idx) {
-        return Container(
-          padding: EdgeInsets.all(16.0),
-          child: Text(_items[idx]),
-        );
-      },
+    return ListView(
+      children: _items,
     );
   }
+
+//相对来说好一点而已
+//  final _items = List<String>.generate(10, (i) => "Item $i");
+//
+//  @override
+//  Widget build(BuildContext context) {
+//    return ListView.builder(
+//      itemCount: 10,
+//      itemBuilder: (context, idx) {
+//        return Container(
+//          padding: EdgeInsets.all(16.0),
+//          child: Text(_items[idx]),
+//        );
+//      },
+//    );
+//  }
 
 }
