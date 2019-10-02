66fc394d0ddaf8b4ddff74b56a996cbfdd4c2248
kason <718087538@qq.com>
2019-10-02 10:43:46 +0800

    可以缓存token

--- lib/test.dart
+++ lib/test.dart
@@ -3,13 +3,14 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:uuid/uuid.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 var uuid = new Uuid();
 
 
 class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    fetchData();
+//    fetchData();
     return Scaffold(
         appBar: AppBar(
           title: Text('测试页'),
@@ -91,8 +92,7 @@ class _MyForm extends State<MyForm> {
 //                print(this._password); //打印输入框的值.密码不用加.text
 
                 Login2();
-
-
+//                huanCun();
               },
               color: Colors.blue,
               textColor: Colors.white,
@@ -104,6 +104,7 @@ class _MyForm extends State<MyForm> {
   }
 }
 
+//获取验证码图片
 fetchData() async {
   var url = 'http://192.168.0.200:7001/api/svg';
   var response = await http.post(url, body: {'uuid': '987654147'});
@@ -117,6 +118,23 @@ fetchData() async {
 Login2() async{
     var url = 'http://192.168.0.200:7001/api/student/login';
     var response = await http.post(url, body: {'mobile': '15989161080','password': '1234567890c'});
-    print('Response status: ${response.statusCode}');
-    print('Response body: ${response.body}');
+//    print('Response status: ${response.statusCode}');
+//    print('Response body: ${response.body}');
+    var data = await jsonDecode(response.body);
+    print('Response body: ${data["data"]["token"]}');
+    String token = data["data"]["token"];
+  //把token缓存起来.
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  await prefs.setString('token', token);
+  await print("查看token"+prefs.get('token'));
+
+}
+
+//设置缓存试试
+huanCun() async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  String counter = "100000000000000";
+//  await prefs.setString('counter', counter);
+
+  await print(prefs.get('counter')+"读取缓存");
 }
