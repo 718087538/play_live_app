adc3d0d2728ae001de5713181ea399efff08afeb
kason <718087538@qq.com>
2019-10-03 18:35:06 +0800

    求助大佬,解决动态拿url

--- lib/playRoom.dart
+++ lib/playRoom.dart
@@ -1,45 +1,42 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:fijkplayer/fijkplayer.dart';
-
 
 class PlayRoom extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-      return Scaffold(
-          appBar: AppBar(
-            title: Text('直播间'),
-          ),
-          body: new VideoScreen());
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('直播间'),
+        ),
+        body: new VideoScreen());
   }
 }
 
 class VideoScreen extends StatefulWidget {
-  String playUrl = "rtmp://202.69.69.180:443/webcast/bshdlive-pc";
-  
-
   @override
   _VideoScreenState createState() => _VideoScreenState();
 }
 
 class _VideoScreenState extends State<VideoScreen> {
   final FijkPlayer player = FijkPlayer();
+  String playUrl = "原始url";
+
   @override
   void initState() {
     super.initState();
-    player.setDataSource(widget.playUrl, autoPlay: true).catchError((e) {
-    });
+    getUrl();
+    player.setDataSource(playUrl, autoPlay: true);
   }
 
   @override
   Widget build(BuildContext context) {
-    return  Container(
-        child: FijkVolumeWatcher(
-            watcher: null,
-            showToast: true,
-            child: FijkView(
-              player: player,
-            )),
+    return Container(
+      child: FijkVolumeWatcher(
+          watcher: null,
+          showToast: true,
+          child: FijkView(
+            player: player,
+          )),
     );
   }
 
@@ -47,5 +44,16 @@ class _VideoScreenState extends State<VideoScreen> {
   void dispose() {
     super.dispose();
     player.release();
+  }
+
+  void test(String newUrl) {
+    setState(() {
+      playUrl = newUrl;
+    });
+  }
+
+  void getUrl() async {
+    String url = "rtmp://202.69.69.180:443/webcast/bshdlive-pc";
+    test(url);
   }
 }
